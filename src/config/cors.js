9e9411a17335313680bcba7ce9951bfcc6a88618
Tiny Devin <trungvan.me@gmail.com>
2025-06-20 10:34:25 +0700

    fix module not found

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,5 @@
 import { WHITELIST_DOMAINS } from '~/utils/constants'
 import { env } from '~/utils/environment'
-import { BusinessLogicError, ERROR_CODES } from '~/utils/apiError.js'
 
 /**
  * Cross origin resource sharing
@@ -18,12 +17,7 @@ export const corsOptions = {
     }
 
     // Throw error if invalid domain
-    return callback(
-      new BusinessLogicError(
-        ERROR_CODES.REQUEST_INVALID,
-        `${origin} not allowed by our CORS Policy.`
-      )
-    )
+    return callback(new Error(`${origin} not allowed by our CORS Policy.`))
   },
 
   // Some legacy browsers (IE11, various SmartTVs) choke on 204
